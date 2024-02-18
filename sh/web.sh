f667a449cfcdf1c2d233c869d5bf018ac75c05e0
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-18 17:23:30 +0000

    No interactivity yet.

--- sh/web.sh
+++ sh/web.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd build
+cd web
 #cd ..
 case "$1" in
 	"deno")
-		LISTEN_PORT=8010 LISTEN_ADDR=127.0.0.1 GRACE_JAIL=1 deno run --allow-read --allow-run ../deno/grace/grace.mjs
+		LISTEN_PORT=8020 LISTEN_ADDR=127.0.0.1 GRACE_JAIL=1 deno run --allow-read --allow-run ../deno/grace/grace.mjs
 		;;
 	*)
-		python3 -m http.server 8010
+		python3 -m http.server 8020
 		;;
 esac
 exit
